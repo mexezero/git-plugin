f1008fef304b2d2bb481c1238f6d6541cfe4ae20
Matthew Behlke <mbehlke@teslamotors.com>
2015-01-15 10:47:48 -0800

    Merge

--- src/main/java/hudson/plugins/git/GitSCM.java
+++ src/main/java/hudson/plugins/git/GitSCM.java
@@ -1059,6 +1059,7 @@ public class GitSCM extends GitSCMBackwardCompatibility {
         
         // Print final summary
         listener.getLogger().println(summary);
+        // Clear summary for next build
         summary = "";
     }
 
